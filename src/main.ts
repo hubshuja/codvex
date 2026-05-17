f8d86beb435ec5d961c0a3b31430e61926775a9f
Shuja <shujauom@gmail.com>
2026-05-17 19:20:08 +0500

    Integrated Super admin registration

--- src/main.ts
+++ src/main.ts
@@ -6,7 +6,11 @@ import { join } from 'path';
 async function bootstrap() {
   const app = await NestFactory.create<NestExpressApplication>(AppModule);
 
-  // Removed setGlobalPrefix - controllers already have /api
+  app.enableCors({
+    origin: ['http://localhost:3000', 'http://localhost:3001', 'https://codvex.net'],
+    credentials: true,
+  });
+
   app.useStaticAssets(join(__dirname, '..', 'client'));
 
   await app.listen(process.env.PORT || 4000);
